f30bd344b6a2a72ef1df602660e9cb2b23bdacfe
Jerish Balakrishnan <jerishbalakrishnan@gmail.com>
2024-12-18 15:52:06 +0530

    Implemented feedback

--- db.php
+++ db.php
@@ -5,8 +5,8 @@ use MysqliDb;
 
 $db = new MysqliDb([
     'host' => 'localhost',
-    'username' => 'root',
-    'password' => '1qYNW7@Zr8fSl',
+    'username' => '',
+    'password' => '',
     'db' => 'alumni_locator',
     'port' => 3306
 ]);
